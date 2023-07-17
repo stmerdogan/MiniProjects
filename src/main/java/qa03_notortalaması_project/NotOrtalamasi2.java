4648a8f7e1c353002dc7f7095606081a16022bca
Mesut Erdogan <mrdogan86@gmail.com>
2023-07-17 20:56:33 +0300

    Mini Projects

--- src/main/java/qa03_notortalaması_project/NotOrtalamasi2.java
+++ src/main/java/qa03_notortalaması_project/NotOrtalamasi2.java
@@ -1,109 +1,122 @@
 package qa03_notortalaması_project;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NotOrtalamasi2 {
     static Scanner scan = new Scanner(System.in);
 
     public static int ortalamahesepla() {
+
         int ortalama = 0;
-        ortalama = (fiziknotualma() + kimyanotualma() + matamatiknotualma() + turkcenotualma() +
-                tarihnotualma() + muziknotualma()) / 6;
-        System.out.println("Not Ortalamanız = " + ortalama);
+            ortalama = (fiziknotualma() + kimyanotualma() + matematiknotualma() + turkcenotualma() +
+                    tarihnotualma() + muziknotualma() / 6);
+            System.out.println("Not Ortalamanız = " + ortalama);
 
-        String sonuc = (ortalama >= 60) ? "geçti " : "kaldı";
-        System.out.println(sonuc);
+            String sonuc = (ortalama >= 60) ? "geçti " : "kaldı";
+            System.out.println(sonuc);
         return ortalama;
-
-    }
-
-    public static int matamatiknotualma() {
-        //Scanner scan = new Scanner(System.in);
-        System.out.println("matematik notunu giriniz =");
-        int a = scan.nextInt();
-
-        if (a < 0 || a > 100) {
-
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-
-            a = b;
-
         }
-        return a;
 
-    }
-
-    public static int kimyanotualma() {
-        //Scanner scan = new Scanner(System.in);
-        System.out.println("Kimya notunu giriniz =");
-        int a = scan.nextInt();
-
-        if (a < 0 || a > 100) {
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-            a = b;
-
-        }
-        return a;
-    }
-
-    private static int fiziknotualma() {
-
-        //Scanner scan = new Scanner(System.in);
-        System.out.println("Fizik notunu giriniz =");
-        int a = scan.nextInt();
-
-        if (a < 0 || a > 100) {
-
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-            a = b;
-
-        }
-        return a;
-
-    }
-
-    private static int turkcenotualma() {
-        // Scanner scan = new Scanner(System.in);
-        System.out.println("Turkce notunu giriniz =");
-        int a = scan.nextInt();
-
-        if (a < 0 || a > 100) {
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-            a = b;
-
-        }
-        return a;
-
-    }
 
     private static int muziknotualma() {
-        // Scanner scan = new Scanner(System.in);
-        System.out.println("Müzik notunu giriniz =");
-        int a = scan.nextInt();
+        int a = 0;
+        try {
+            System.out.println("Müzik notunu giriniz =");
+            a = scan.nextInt();
 
-        if (a < 0 || a > 100) {
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-            a = b;
-
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = muziknotualma();
         }
         return a;
     }
 
     private static int tarihnotualma() {
-        //Scanner scan = new Scanner(System.in);
-        System.out.println("Tarih notunu giriniz =");
-        int a = scan.nextInt();
+        int a = 0;
+        try {
+            System.out.println("Tarih notunu giriniz =");
+            a = scan.nextInt();
 
-        if (a < 0 || a > 100) {
-            System.out.println("0 ila 100 arası bir not giriniz");
-            int b = scan.nextInt();
-            a = b;
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = tarihnotualma();
+        }
+        return a;
+    }
 
+    private static int turkcenotualma() {
+        int a = 0;
+        try {
+            System.out.println("Türkçe notunu giriniz =");
+            a = scan.nextInt();
+
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = turkcenotualma();
+        }
+        return a;
+    }
+
+    private static int fiziknotualma() {
+        int a = 0;
+        try {
+            System.out.println("Fizik notunu giriniz =");
+            a = scan.nextInt();
+
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = fiziknotualma();
+        }
+        return a;
+    }
+
+    public static int matematiknotualma() {
+        int a = 0;
+        try {
+            System.out.println("Matematik notunu giriniz =");
+            a = scan.nextInt();
+
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = matematiknotualma();
+        }
+        return a;
+    }
+
+    public static int kimyanotualma() {
+        int a = 0;
+        try {
+            System.out.println("Kimya notunu giriniz =");
+            a = scan.nextInt();
+
+            if (a < 0 || a > 100) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! 0 ila 100 arasında bir not giriniz.");
+            scan.nextLine();
+            a = kimyanotualma();
         }
         return a;
     }
